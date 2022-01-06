f91a94af109c3d2e8619f2aa326ad05e1490eb2b
willianCaldas <willian.caldas@hotmail.com>
2022-01-06 11:29:53 -0300

    Versão 0.001 BossBank Web Aplication

---
+++ BossBank.Data/Interfaces/IRepositoryModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BossBank.Data.Interfaces
+{
+    internal interface IRepositoryModel<T> where T : class
+    {
+        List<T> GetAll();
+        T GetPk(params object[] varT);
+        T Add(T item);
+        T Update(T item);
+        void Delete(T item);
+        void Delete(params object[] varT);
+        void SaveChanges();
+    }
+}
+
+namespace BossBank.Data
+{
+    public class IReposirotyModel
+    {
+    }
+}
---
+++ BossBank.Data/Repositories/RepositoryBase.cs
@@ -0,0 +1,82 @@
+﻿using BossBank.Data.Interfaces;
+using BossBank.Data.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BossBank.Data.Repositories
+{
+    public class RepositoryBase<T> : IRepositoryModel<T>, IDisposable where T : class
+    {
+        protected BossbankDBContext _BossBankContexto;
+        public bool _SaveChanges = true;
+
+        public RepositoryBase(bool saveChanges = true)
+        {
+            _SaveChanges = saveChanges;
+            _BossBankContexto = new BossbankDBContext();
+        }
+
+        public T Add(T item)
+        {
+            _BossBankContexto.Set<T>().Add(item);
+
+            if (_SaveChanges)
+            {
+                _BossBankContexto.SaveChanges();
+            }
+
+            return item;
+        }
+
+        public void Delete(T item)
+        {
+            _BossBankContexto.Set<T>().Remove(item);
+
+            if (_SaveChanges)
+            {
+                _BossBankContexto.SaveChanges();
+            }
+        }
+
+        public void Delete(params object[] varT)
+        {
+            var obj = GetPk(varT);
+            Delete(obj);
+        }
+
+        public void Dispose()
+        {
+            _BossBankContexto.Dispose();
+        }
+
+        public List<T> GetAll()
+        {
+            return _BossBankContexto.Set<T>().ToList();
+        }
+
+        public T GetPk(params object[] varT)
+        {
+            return _BossBankContexto.Set<T>().Find(varT);
+        }
+
+        public void SaveChanges()
+        {
+            _BossBankContexto.SaveChanges();
+        }
+
+        public T Update(T item)
+        {
+            _BossBankContexto.Entry(item).State = EntityState.Modified;
+
+            if (_SaveChanges)
+            {
+                _BossBankContexto.SaveChanges();
+            }
+            return item;
+        }
+    }
+}
