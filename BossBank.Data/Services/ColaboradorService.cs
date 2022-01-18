4b0725db8f1991c3429909289f15c31eef39555e
willianCaldas <willian.caldas@hotmail.com>
2022-01-18 11:39:19 -0300

    BBWA v0.011 - Inclusão do Controller Colaborador, da Classe ColaboradorService e da View para a Lista de Colaboradores

---
+++ BossBank.Data/Services/ColaboradorService.cs
@@ -0,0 +1,19 @@
+﻿using BossBank.Data.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BossBank.Data.Services
+{
+    public class ColaboradorService
+    {
+        public RepositoryColaborador RepColaborador { get; set; }
+
+        public ColaboradorService()
+        {
+            RepColaborador = new RepositoryColaborador();
+        }
+    }
+}
---
+++ BossBank.Web/Controllers/ColaboradorController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using BossBank.Data.Services;
+using BossBank.Data.Models;
+using System.Collections.Generic;
+
+namespace BossBank.Web.Controllers
+{
+    public class ColaboradorController : Controller
+    {
+        private ColaboradorService ServColaborador { get; set; }
+
+        public ColaboradorController()
+        {
+            ServColaborador = new ColaboradorService();
+        }
+
+        public IActionResult Index()
+        {
+            List<Colaborador> listColaborador = ServColaborador.RepColaborador.GetAll();
+            return View(listColaborador);
+        }
+    }
+}
